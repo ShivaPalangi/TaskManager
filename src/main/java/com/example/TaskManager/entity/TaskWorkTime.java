16acf9420996bbc75249bd62197fa6fca01379be
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-07-31 01:33:13 +0330

    Add TaskWorkTime Entity

---
+++ src/main/java/com/example/TaskManager/entity/TaskWorkTime.java
@@ -0,0 +1,25 @@
+package com.example.TaskManager.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "task_work_time")
+@Data
+public class TaskWorkTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    @ManyToOne(cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "task", nullable = false)
+    private Task task;
+}
