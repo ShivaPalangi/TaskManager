6361206c4bf15b3fc93c9f6f7c1fcfaf4447998c
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-07-31 01:26:59 +0330

    Add Employee Entity

---
+++ src/main/java/com/example/TaskManager/entity/Employee.java
@@ -0,0 +1,37 @@
+package com.example.TaskManager.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "employee")
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(unique = true)
+    private String emailAddress;
+
+    private LocalDate dateOfBirth;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "company")
+    private Company company;
+
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
+    private List<Membership> memberships;
+}
