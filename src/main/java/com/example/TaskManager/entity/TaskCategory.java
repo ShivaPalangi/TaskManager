4e4fd85317948d6417dfd0d4162f948a9ba45d41
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-07-31 01:31:50 +0330

    Add TaskCategory Entity

---
+++ src/main/java/com/example/TaskManager/entity/TaskCategory.java
@@ -0,0 +1,23 @@
+package com.example.TaskManager.entity;
+
+import com.example.TaskManager.enums.TaskCategories;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Entity
+@Table(name = "task_category")
+@Data
+public class TaskCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskCategories category;
+
+    @OneToMany(mappedBy = "taskCategory", cascade = CascadeType.ALL)
+    private List<Task> tasks;
+}
