60301af589bf7a0e0b52e478cdc22e605a4bbfff
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-07-31 22:43:35 +0330

    Add TaskCategory Mapper

---
+++ src/main/java/com/example/TaskManager/mapper/TaskCategoryMapper.java
@@ -0,0 +1,30 @@
+package com.example.TaskManager.mapper;
+
+import com.example.TaskManager.dto.TaskCategoryDTO;
+import com.example.TaskManager.entity.TaskCategory;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Component
+public class TaskCategoryMapper {
+    public static TaskCategoryDTO mapToTaskCategoryDTO(TaskCategory taskCategory){
+        TaskCategoryDTO taskCategoryDTO = new TaskCategoryDTO();
+        taskCategoryDTO.setId(taskCategory.getId());
+        taskCategoryDTO.setCategory(taskCategory.getCategory());
+        if ( ! taskCategory.getTasks().isEmpty())
+            taskCategoryDTO.setTasks(taskCategory.getTasks().stream().map(TaskMapper::mapToTaskDTO).collect(Collectors.toList()));
+        return taskCategoryDTO;
+    }
+
+
+    public static TaskCategory mapToTaskCategoryEntity(TaskCategoryDTO taskCategoryDTO){
+        TaskCategory taskCategory = new TaskCategory();
+        taskCategory.setId(taskCategoryDTO.getId());
+        taskCategory.setCategory(taskCategoryDTO.getCategory());
+        if ( ! taskCategoryDTO.getTasks().isEmpty())
+            taskCategory.setTasks(taskCategoryDTO.getTasks().stream().map(TaskMapper::mapToTaskEntity).collect(Collectors.toList()));
+        return taskCategory;
+    }
+}
