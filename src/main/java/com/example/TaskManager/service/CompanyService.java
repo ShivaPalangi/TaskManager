c4c72b4e4f835295b018be8799efd2b9c4251029
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-09-05 19:48:25 +0330

    Update Service Classes

--- src/main/java/com/example/TaskManager/service/CompanyService.java
+++ src/main/java/com/example/TaskManager/service/CompanyService.java
@@ -1,11 +1,48 @@
 package com.example.TaskManager.service;
 
 import com.example.TaskManager.dto.CompanyDTO;
+import com.example.TaskManager.entity.Company;
+import com.example.TaskManager.exception.ResourceNotFoundException;
+import com.example.TaskManager.mapper.CompanyMapper;
+import com.example.TaskManager.repository.CompanyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface CompanyService {
-    CompanyDTO addCompany(CompanyDTO companyDTO);
 
-    CompanyDTO updateCompany(CompanyDTO companyDTO);
+@Service
+@RequiredArgsConstructor
+public class CompanyService {
 
-    CompanyDTO getCompany(Long id);
+    private CompanyRepository companyRepository;
+
+
+    public CompanyDTO addCompany(CompanyDTO companyDTO) {
+        Company company = CompanyMapper.mapToCompanyEntity(companyDTO);
+        company = companyRepository.save(company);
+        return CompanyMapper.mapToCompanyDTO(company);
+    }
+
+
+    public CompanyDTO updateCompany(CompanyDTO companyDTO) {
+        Company companyToUpdate = companyRepository.findById(companyDTO.getId()).orElseThrow(
+                () -> new ResourceNotFoundException("Company", "Id", companyDTO.getId()));
+        updateCompanyEntityFromDTO(companyToUpdate, companyDTO);
+        Company savedCompany = companyRepository.save(companyToUpdate);
+        return CompanyMapper.mapToCompanyDTO(savedCompany);
+    }
+
+
+    public CompanyDTO getCompany(Long id) {
+        Company company = companyRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Company", "Id", id));
+        return CompanyMapper.mapToCompanyDTO(company);
+    }
+
+    private void updateCompanyEntityFromDTO(Company companyToUpdate, CompanyDTO companyDTO) {
+        if (companyDTO.getName() != null)
+            companyToUpdate.setName(companyDTO.getName());
+        if (companyDTO.getDescription() != null)
+            companyToUpdate.setDescription(companyDTO.getDescription());
+    }
+
 }
--- src/main/java/com/example/TaskManager/service/TeamService.java
+++ src/main/java/com/example/TaskManager/service/TeamService.java
@@ -1,11 +1,54 @@
 package com.example.TaskManager.service;
 
 import com.example.TaskManager.dto.TeamDTO;
+import com.example.TaskManager.entity.Company;
+import com.example.TaskManager.entity.Team;
+import com.example.TaskManager.exception.ResourceNotFoundException;
+import com.example.TaskManager.mapper.TeamMapper;
+import com.example.TaskManager.repository.CompanyRepository;
+import com.example.TaskManager.repository.TeamRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface TeamService {
-    TeamDTO updateTeam(TeamDTO teamDTO);
 
-    TeamDTO addTeam(TeamDTO teamDTO, Long teamId);
+@Service
+@RequiredArgsConstructor
+public class TeamService  {
+    private TeamRepository teamRepository;
+    private CompanyRepository companyRepository;
 
-    TeamDTO getTeam(Long teamId);
+
+
+    public TeamDTO addTeam(TeamDTO teamDTO, Long id) {
+        Company company = companyRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Company", "Id", id));
+        Team team = TeamMapper.mapToTeamEntity(teamDTO);
+        team.setCompany(company);
+        Team savedTeam = teamRepository.save(team);
+        return TeamMapper.mapToTeamDTO(savedTeam);
+    }
+
+
+    public TeamDTO getTeam(Long teamId) {
+        Team team = teamRepository.findById(teamId).orElseThrow(
+                () -> new ResourceNotFoundException("Team", "Id", teamId));
+        return TeamMapper.mapToTeamDTO(team);
+    }
+
+
+
+    public TeamDTO updateTeam(TeamDTO teamDTO) {
+        Team teamToUpdate = teamRepository.findById(teamDTO.getId()).orElseThrow(
+                () -> new ResourceNotFoundException("Team", "Id", teamDTO.getId()));
+        UpdateTeamEntityFromDTO(teamToUpdate, teamDTO);
+        Team savedTeam = teamRepository.save(teamToUpdate);
+        return TeamMapper.mapToTeamDTO(savedTeam);
+    }
+
+    private void UpdateTeamEntityFromDTO(Team teamToUpdate, TeamDTO teamDTO) {
+        if (teamDTO.getName() != null)
+            teamToUpdate.setName(teamDTO.getName());
+        if (teamDTO.getDescription() != null)
+            teamToUpdate.setDescription(teamDTO.getDescription());
+    }
 }
