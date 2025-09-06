7ae781ae2655975794a0a399cf286ae5b838564c
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-09-06 12:57:43 +0330

    Add RegisterRequest Class

---
+++ src/main/java/com/example/TaskManager/dto/RegisterRequest.java
@@ -0,0 +1,24 @@
+package com.example.TaskManager.dto;
+
+import com.example.TaskManager.enums.Role;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    @NotBlank(message = "Firstname is required")
+    private String firstname;
+    @NotBlank(message = "Lastname is required")
+    private String lastname;
+    @NotBlank(message = "Email is required")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private String password;
+    private Role role;
+}
