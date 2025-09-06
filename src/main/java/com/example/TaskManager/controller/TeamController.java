833dc9e846da877e8316499ab652ce62e155bdc9
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-09-06 13:38:27 +0330

    Update Team Controller

--- src/main/java/com/example/TaskManager/controller/TeamController.java
+++ src/main/java/com/example/TaskManager/controller/TeamController.java
@@ -1,30 +1,29 @@
 package com.example.TaskManager.controller;
 
-import com.example.TaskManager.dto.CompanyDTO;
 import com.example.TaskManager.dto.TeamDTO;
 import com.example.TaskManager.service.CompanyService;
 import com.example.TaskManager.service.TeamService;
-import lombok.AllArgsConstructor;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @RestController
 @RequestMapping("company")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class TeamController {
     private TeamService teamService;
     private CompanyService companyService;
 
     @PostMapping("{id}/add-team")
-    public ResponseEntity<TeamDTO> addTeam(@RequestBody TeamDTO teamDTO, @PathVariable Long id){
+    public ResponseEntity<TeamDTO> addTeam(@Valid @RequestBody TeamDTO teamDTO, @PathVariable Long id){
         TeamDTO createdTeam = teamService.addTeam(teamDTO, id);
         return new ResponseEntity<>(createdTeam, HttpStatus.CREATED);
     }
 
     @PatchMapping("{companyId}/team/{teamId}/update")
-    public ResponseEntity<TeamDTO> updateTeam(@RequestBody TeamDTO teamDTO, @PathVariable("companyId") Long companyId, @PathVariable("teamId") Long teamId){
+    public ResponseEntity<TeamDTO> updateTeam(@Valid @RequestBody TeamDTO teamDTO, @PathVariable("companyId") Long companyId, @PathVariable("teamId") Long teamId){
         teamDTO.setId(teamId);
         TeamDTO updatedTeam = teamService.updateTeam(teamDTO);
         return new ResponseEntity<>(updatedTeam, HttpStatus.OK);
