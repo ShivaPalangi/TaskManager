efb4300c7425f9df7b000419dde0087327ff90a4
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-09-06 23:19:43 +0330

    Update Team Controller

--- src/main/java/com/example/TaskManager/controller/TeamController.java
+++ src/main/java/com/example/TaskManager/controller/TeamController.java
@@ -4,6 +4,7 @@ import com.example.TaskManager.dto.TeamDTO;
 import com.example.TaskManager.service.CompanyService;
 import com.example.TaskManager.service.TeamService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,20 +18,20 @@ public class TeamController {
     private CompanyService companyService;
 
     @PostMapping("{id}/add-team")
-    public ResponseEntity<TeamDTO> addTeam(@Valid @RequestBody TeamDTO teamDTO, @PathVariable Long id){
+    public ResponseEntity<TeamDTO> addTeam(@Valid @RequestBody TeamDTO teamDTO, @PathVariable @Min(1) Long id){
         TeamDTO createdTeam = teamService.addTeam(teamDTO, id);
         return new ResponseEntity<>(createdTeam, HttpStatus.CREATED);
     }
 
     @PatchMapping("{companyId}/team/{teamId}/update")
-    public ResponseEntity<TeamDTO> updateTeam(@Valid @RequestBody TeamDTO teamDTO, @PathVariable("companyId") Long companyId, @PathVariable("teamId") Long teamId){
+    public ResponseEntity<TeamDTO> updateTeam(@Valid @RequestBody TeamDTO teamDTO, @PathVariable("companyId") @Min(1) Long companyId, @PathVariable("teamId") @Min(1) Long teamId){
         teamDTO.setId(teamId);
         TeamDTO updatedTeam = teamService.updateTeam(teamDTO);
         return new ResponseEntity<>(updatedTeam, HttpStatus.OK);
     }
 
     @GetMapping("{companyId}/team/{teamId}")
-    public  ResponseEntity<TeamDTO> getTeam(@PathVariable("companyId") Long companyId, @PathVariable("teamId") Long teamId){
+    public  ResponseEntity<TeamDTO> getTeam(@PathVariable("companyId") @Min(1) Long companyId, @PathVariable("teamId") @Min(1) Long teamId){
         TeamDTO teamDTO = teamService.getTeam(teamId);
         return new ResponseEntity<>(teamDTO, HttpStatus.OK);
     }
