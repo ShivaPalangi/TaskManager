09449205ef8df51545473d89567b0f2fe79ab50e
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-09-13 22:43:28 +0330

    Update User Controller

--- src/main/java/com/example/TaskManager/controller/UserController.java
+++ src/main/java/com/example/TaskManager/controller/UserController.java
@@ -2,11 +2,13 @@ package com.example.TaskManager.controller;
 
 import com.example.TaskManager.dto.ChangePasswordRequest;
 import com.example.TaskManager.dto.UserDTO;
+import com.example.TaskManager.entity.User;
 import com.example.TaskManager.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,13 +24,13 @@ public class UserController {
     private final UserService userService;
 
     @PatchMapping("change-password")
-    public ResponseEntity<UserDTO> changePassword(@Valid @RequestBody ChangePasswordRequest request, Principal connectedUser) {
-        UserDTO user = userService.changePassword(request, connectedUser);
+    public ResponseEntity<UserDTO> changePassword(@Valid @RequestBody ChangePasswordRequest request, @AuthenticationPrincipal User currentUser) {
+        UserDTO user = userService.changePassword(request, currentUser);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
     @PatchMapping("update-user-info")
-    public ResponseEntity<UserDTO> updateUserInfo(@Valid @RequestBody UserDTO userDTO, Principal connectedUser) {
-        UserDTO updatedUser = userService.updateUser(userDTO, connectedUser);
+    public ResponseEntity<UserDTO> updateUserInfo(@Valid @RequestBody UserDTO userDTO, @AuthenticationPrincipal User currentUser) {
+        UserDTO updatedUser = userService.updateUser(userDTO, currentUser);
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 }
