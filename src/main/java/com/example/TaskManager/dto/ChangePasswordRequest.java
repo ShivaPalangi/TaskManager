23b210523e7714784ff714f37a6ddbe8f379d9ec
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-09-13 17:40:52 +0330

    Update ChangePasswordRequest

--- src/main/java/com/example/TaskManager/dto/ChangePasswordRequest.java
+++ src/main/java/com/example/TaskManager/dto/ChangePasswordRequest.java
@@ -1,5 +1,6 @@
 package com.example.TaskManager.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +9,10 @@ import lombok.Setter;
 @Setter
 @Builder
 public class ChangePasswordRequest {
+    @NotBlank(message = "Current Password is required")
     private String currentPassword;
+    @NotBlank(message = "New Password is required")
     private String newPassword;
+    @NotBlank(message = "Confirmation Password is required")
     private String confirmationPassword;
 }
