114e3f1e0be9f02fb043a19dbdd7f08404c9344d
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-09-13 23:01:28 +0330

    Update User Entity

--- src/main/java/com/example/TaskManager/entity/User.java
+++ src/main/java/com/example/TaskManager/entity/User.java
@@ -44,12 +44,11 @@ public class User implements UserDetails {
     @OneToMany(mappedBy = "user")
     private List<Token> tokens;
 
-    @ManyToOne
-    @JoinColumn(name = "company")
-    private Company company;
-
     @OneToMany(mappedBy = "employee")
     private List<Membership> memberships;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Company> companies;
 
 
     @Override
