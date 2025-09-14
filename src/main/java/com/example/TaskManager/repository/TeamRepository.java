909c4792dde82feea0241b0fc343f88b1294b7dd
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-09-14 12:35:39 +0330

    Update Team Repository

--- src/main/java/com/example/TaskManager/repository/TeamRepository.java
+++ src/main/java/com/example/TaskManager/repository/TeamRepository.java
@@ -4,6 +4,9 @@ import com.example.TaskManager.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    Optional<Team> findByIdAndCompanyId(Long id, Long companyId);
 }
