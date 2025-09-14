ae5297a8506a4e04908d147a5255a2543492003c
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-09-14 12:55:52 +0330

    Update Membership DTO

--- src/main/java/com/example/TaskManager/dto/MembershipDTO.java
+++ src/main/java/com/example/TaskManager/dto/MembershipDTO.java
@@ -10,16 +10,10 @@ import java.util.List;
 @Data
 public class MembershipDTO {
     private Long id;
-
-    @NotBlank(message = "Position is required", groups = ValidationGroups.Create.class)
-    private String position;
-
     @NotNull(message = "Role is required", groups = ValidationGroups.Create.class)
     private MembershipRoles role;
-
     @NotNull(message = "Team Id is required", groups = ValidationGroups.Create.class)
     private Long teamId;
-
     @NotNull(message = "Employee Id is required", groups = ValidationGroups.Create.class)
     private Long employeeId;
     private List<TaskDTO> assignedTasks;
