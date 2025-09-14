6dc5a0157ddb434bdf38539e336e15240e8cf959
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-09-14 12:14:27 +0330

    Update Team DTO

--- src/main/java/com/example/TaskManager/dto/TeamDTO.java
+++ src/main/java/com/example/TaskManager/dto/TeamDTO.java
@@ -2,7 +2,6 @@ package com.example.TaskManager.dto;
 
 import com.example.TaskManager.validation.ValidationGroups;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import java.util.List;
@@ -10,14 +9,10 @@ import java.util.List;
 @Data
 public class TeamDTO {
     private Long id;
-
     @NotBlank(message = "Name is required", groups = ValidationGroups.Create.class)
     @Size(min = 3, max = 50, message = "Title must be between 3 and 50 characters")
     private String name;
-
-    @NotNull(message = "Company id is required", groups = ValidationGroups.Create.class)
     private Long companyId;
-
     private String description;
     private List<MembershipDTO> memberships;
 }
