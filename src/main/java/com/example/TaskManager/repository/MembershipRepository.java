0a02cd355484048b1c912bf81a5c5434ee264842
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-09-14 10:16:17 +0330

    Update Membership Repository

--- src/main/java/com/example/TaskManager/repository/MembershipRepository.java
+++ src/main/java/com/example/TaskManager/repository/MembershipRepository.java
@@ -1,9 +1,16 @@
 package com.example.TaskManager.repository;
 
 import com.example.TaskManager.entity.Membership;
+import com.example.TaskManager.entity.Team;
+import com.example.TaskManager.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
+    Optional<Membership> findByEmployeeAndTeam(User employee, Team team);
+
+    boolean existsByEmployeeIdAndTeamCompanyId(Long employeeId, Long companyId);
 }
