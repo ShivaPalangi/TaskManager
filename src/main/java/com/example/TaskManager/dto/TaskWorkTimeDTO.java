7da05e75c5396dfb27d1b7ca0faca5b021aad1fb
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-09-16 00:21:25 +0330

    Update TaskWorkTime DTO

--- src/main/java/com/example/TaskManager/dto/TaskWorkTimeDTO.java
+++ src/main/java/com/example/TaskManager/dto/TaskWorkTimeDTO.java
@@ -1,14 +1,12 @@
 package com.example.TaskManager.dto;
 
 import com.example.TaskManager.validation.ValidationGroups;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class TaskWorkTimeDTO {
     private Long id;
-    @NotBlank(message = "Start time is required", groups = ValidationGroups.Create.class)
     private String startTime;
     private String endTime;
     @NotNull(message = "Task id is required", groups = ValidationGroups.Create.class)
