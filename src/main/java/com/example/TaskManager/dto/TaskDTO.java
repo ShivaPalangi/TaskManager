0fad5802e3f0851ee894a39f72db69e07276ac56
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-09-20 06:43:36 +0330

    Update Task DTO

--- src/main/java/com/example/TaskManager/dto/TaskDTO.java
+++ src/main/java/com/example/TaskManager/dto/TaskDTO.java
@@ -2,21 +2,16 @@ package com.example.TaskManager.dto;
 
 import com.example.TaskManager.enums.TaskPriorities;
 import com.example.TaskManager.enums.TaskStatusTypes;
-import com.example.TaskManager.validation.ValidationGroups;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 import java.util.List;
 
 @Data
 public class TaskDTO {
     private Long id;
-
-    @NotBlank(message = "Name is required", groups = ValidationGroups.Create.class)
-    @Size(min = 3, max = 50, message = "Title must be between 3 and 50 characters")
+    @NotBlank(message = "Name is required")
     private String name;
-
     private String description;
     private String startTime;
     private String endTime;
@@ -25,11 +20,8 @@ public class TaskDTO {
     private TaskPriorities priority;
     private TaskStatusTypes taskStatus;
     private Long taskCategoryId;
-
-    @NotNull(message = "Responsible Id is required", groups = ValidationGroups.Create.class)
+    @NotNull(message = "Responsible Id is required")
     private Long responsibleId;
-
     private Long creatorId;
-
     private List<TaskWorkTimeDTO> workTimes;
 }
