af039926413e00e180956af568e222ae05444b82
Shiva Palangi <199654582+ShivaPalangi@users.noreply.github.com>
2025-09-20 06:44:03 +0330

    Update TaskWorkTime Entity

--- src/main/java/com/example/TaskManager/entity/TaskWorkTime.java
+++ src/main/java/com/example/TaskManager/entity/TaskWorkTime.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Entity
@@ -13,18 +11,12 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
 public class TaskWorkTime {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @CreatedDate
-    @Column(updatable = false, nullable = false)
     private LocalDateTime startTime;
-
-    @Column(insertable = false, updatable = false)
     private LocalDateTime endTime;
 
     @ManyToOne(optional = false)
